a1db5bd6b44a7a9674c83315df64f9254ba5c554
Nicholas Le <nle031@west-mec.org>
2019-02-26 18:19:36 -0700

    Final Commit
    Added the dates and events to the calendar while also highlighting the current day.
    Ready for review.

--- lht_calendar.js
+++ lht_calendar.js
@@ -32,7 +32,7 @@
 /*
 set the date displayed in the calendar
 */
-var thisDay = new Date("August 24, 2018");
+var thisDay = new Date();
 
 //Write the calendar to the element with the id of "calendar"
 document.getElementById("calendar").innerHTML = createCalendar(thisDay);
@@ -42,6 +42,7 @@ function createCalendar(calDate) {
       var calendarHTML = "<table id='calendar_table'>";
       calendarHTML += calCaption(calDate);
       calendarHTML += calWeekdayRow();
+      calendarHTML += calDays(calDate);
       calendarHTML += "</table>";
       return calendarHTML;
 }
@@ -87,9 +88,45 @@ function daysInMonth(calDate) {
 
       //Revise the days in february for leap years
       if (thisYear % 4 === 0) {
-            dayCount[1] = 29;
+            if ((thisYear % 100 != 0) || (thisYear % 400 === 0)) {
+                  dayCount[1] = 29;
+            }
       }
 
       //Return the # of days for the current month
       return dayCount[thisMonth];
+}
+//Function to write the table rows for each day of the month
+function calDays(calDate) {
+      //Determine the starting day of the month
+      var day = new Date(calDate.getFullYear(), calDate.getMonth(), 1);
+      var weekDay = day.getDay();
+      //Write blank cells preceding starting day
+      var htmlCode = "<tr>";
+      for (var i = 0; i < weekDay; i++) {
+            htmlCode += "<td></td>";
+      }
+      //Write cells for each day of the month
+      var totalDays = daysInMonth(calDate);
+      var hightlightDay = calDate.getDate();
+      for (var i = 1; i <= totalDays; i++) {
+            day.setDate(i);
+            weekDay = day.getDay();
+
+            //if statements
+            if (weekDay === 0) {
+                  htmlCode += "<tr>";
+            }
+
+            if (i === hightlightDay) {
+                  htmlCode += "<td class='calendar_dates' id='calendar_today'>" + i + dayEvent[i] + "</td>"
+            } else {
+                  htmlCode += "<td class='calendar_dates'>" + i + dayEvent[i] + "</td>";
+            }
+
+            if (weekDay === 6) {
+                  htmlCode += "</tr>";
+            }
+      }
+      return htmlCode;
 }
